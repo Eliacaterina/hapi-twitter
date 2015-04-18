ecd7bac14ec542e9f6f0f01c07ac34e87e58588e
Your Name <you@example.com>
2015-04-18 23:23:41 +0800

    DELTE added

--- routes/tweets.js
+++ routes/tweets.js
@@ -56,7 +56,63 @@ server.route([
 	      }
 	    }
 	  }
-	}
+	},
+	{
+	  method: 'GET',
+	  path:'/tweets/{id}',
+	  handler: function(request, reply) {
+	  	var tweet_id = encodeURIComponent(request.params.id); //i dont understand the encodeURIComponent
+	  	var db       = request.server.plugins['hapi-mongodb'].ObjectID;
+
+	  	db.collection('tweets').findOne({ "_id": ObjectId(tweet_id)}, function(err, tweet) {
+	  		if (err) { return reply('Internal MongoDB error', err); }
+
+	  		reply(tweet);
+	  	})
+	  }
+	},
+	{
+  // Delete one tweet
+  method: 'DELETE',
+  path: '/tweets/{id}',
+  handler: function(request, reply) {
+    Auth.authenticated(request, function(result) {
+      if (result.authenticated) {
+        var tweet_id = encodeURIComponent(request.params.id);
+
+        var db = request.server.plugins['hapi-mongodb'].db;
+        var ObjectId = request.server.plugins['hapi-mongodb'].ObjectID;
+
+        db.collection('tweets').remove({ "_id": ObjectId(tweet_id) }, function(err, writeResult) {
+          if (err) { return reply('Internal MongoDB error', err); }
+
+          reply(writeResult);
+        });
+      } else {
+        reply(result.message);
+      }
+    });
+  }
+},
+{
+  // Retrieve all tweets by a specific user
+  method: 'GET',
+  path: '/users/{username}/tweets',
+  handler: function(request, reply) {
+    var db = request.server.plugins['hapi-mongodb'].db;
+    var username = encodeURIComponent(request.params.username);
+
+    db.collection('users').findOne({ "username": username }, function(err, user) {
+      if (err) { return reply('Internal MongoDB error', err); }
+
+      db.collection('tweets').find({ "user_id": user._id }).toArray(function(err, tweets) {
+        if (err) { return reply('Internal MongoDB error', err); }
+
+        reply(tweets);
+      });
+    })
+  }
+}
 ]);
 
 
